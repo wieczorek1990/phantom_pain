ab7f6b38ffbce5591cb4d2d820779efea4d0a7ec
Łukasz Wieczorek <lwieczorek@apptension.com>
2016-03-04 12:39:35 +0100

    Initialize

---
+++ lib/phantom_pain.rb
@@ -0,0 +1,72 @@
+require 'phantomjs'
+require 'selenium-webdriver'
+require 'tor'
+require 'webdriver-user-agent'
+
+class Tor::Controller
+  def signal(name)
+    send_command :signal, name
+    read_reply
+  end
+
+  def restart
+    signal :newnym
+  end
+end
+
+module Webdriver
+  module UserAgent
+    module Devices
+      public :random_user_agent
+    end
+  end
+end
+
+class WebDriverFactory
+  def initialize(proxy_host, proxy_port)
+    Selenium::WebDriver::PhantomJS.path = Phantomjs.path
+    @user_agents = Class.new.extend(Webdriver::UserAgent::Devices)
+    @base_desired_capabilities = {
+      'phantomjs.cli.args' => ["--proxy=#{proxy_host}:#{proxy_port}", '--proxy-type=socks5']
+    }
+  end
+
+  def random_user_agent
+    @user_agents.random_user_agent.chomp
+  end
+
+  def create
+    desired_capabilities = {
+      'phantomjs.page.settings.userAgent' => random_user_agent
+    }
+    desired_capabilities.merge! @base_desired_capabilities
+    Selenium::WebDriver.for :phantomjs, desired_capabilities: desired_capabilities
+  end
+end
+
+class CookieTorController < Tor::Controller
+  def initialize(control_auth_cookie_path, options = {}, &block)
+    cookie = `hexdump -e '32/1 "%02x""\n"' #{control_auth_cookie_path}`.chomp
+    options[:cookie] = cookie
+    super(options, block)
+  end
+end
+
+class PhantomPainFactory
+  def initialize(control_auth_cookie_path, proxy_host, proxy_port)
+    @tor_controller = CookieTorController.new control_auth_cookie_path
+    @web_driver_factory = WebDriverFactory.new(proxy_host, proxy_port)
+  end
+
+  def create(&block)
+    web_driver = @web_driver_factory.create
+    block.call web_driver
+    web_driver.quit
+    @tor_controller.restart
+  end
+
+  def relieve
+    @tor_controller.quit
+  end
+end
+
---
+++ phantom_pain.gemspec
@@ -0,0 +1,17 @@
+Gem::Specification.new do |s|
+  s.name        = 'phantom_pain'
+  s.version     = '0.0.1'
+  s.date        = '2016-03-04'
+  s.summary     = "Phantom Pain"
+  s.description = "Bot factory with PhantomJS, Selenium and Tor"
+  s.authors     = ["Łukasz Wieczorek"]
+  s.email       = 'wieczorek1990@gmail.com'
+  s.files       = ["lib/phantom_pain.rb"]
+  s.homepage    = "https://github.com/wieczorek1990/phantom_pain"
+  s.license     = 'GPL-3.0'
+  s.add_runtime_dependency "phantomjs", "~> 0"
+  s.add_runtime_dependency "selenium-webdriver", "~> 0"
+  s.add_runtime_dependency "tor", "~> 0"
+  s.add_runtime_dependency "webdriver-user-agent", "~> 0"
+end
+
