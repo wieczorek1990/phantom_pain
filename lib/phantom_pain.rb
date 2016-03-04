a134b3619f3c500ccb096408c3beaf695380549d
Łukasz Wieczorek <lwieczorek@apptension.com>
2016-03-04 12:56:54 +0100

    Fixed block passing

--- lib/phantom_pain.rb
+++ lib/phantom_pain.rb
@@ -48,7 +48,7 @@ class CookieTorController < Tor::Controller
   def initialize(control_auth_cookie_path, options = {}, &block)
     cookie = `hexdump -e '32/1 "%02x""\n"' #{control_auth_cookie_path}`.chomp
     options[:cookie] = cookie
-    super(options, block)
+    super(options, &block)
   end
 end
 
