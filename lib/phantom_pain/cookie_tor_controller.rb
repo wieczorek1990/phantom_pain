88aad3d7a3fc6d9d593787d281e54aca44f165a0
Łukasz Wieczorek <wieczorek1990@gmail.com>
2019-06-07 19:23:25 +0200

    Update cookie_tor_controller.rb

--- lib/phantom_pain/cookie_tor_controller.rb
+++ lib/phantom_pain/cookie_tor_controller.rb
@@ -2,8 +2,8 @@ require_relative 'ext/tor_controller'
 
 class CookieTorController < Tor::Controller
   def initialize(control_auth_cookie_path, options = {}, &block)
-    cookie = `hexdump -e '32/1 "%02x""\n"' #{control_auth_cookie_path}`.chomp
-    options[:cookie] = cookie
+    cookie = `hexdump -e '32/1 "%02x""\n"' #{control_auth_cookie_path}`
+    options[:cookie] = cookie.chomp
     super(options, &block)
   end
 end
