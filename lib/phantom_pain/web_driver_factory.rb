b188d85c68dceae46bda9a8990d13504464d9c49
Łukasz Wieczorek <wieczorek1990@gmail.com>
2019-06-10 10:06:06 +0200

    Update web_driver_factory.rb

--- lib/phantom_pain/web_driver_factory.rb
+++ lib/phantom_pain/web_driver_factory.rb
@@ -7,21 +7,13 @@ class WebDriverFactory
   def initialize(proxy_host, proxy_port)
     Selenium::WebDriver::PhantomJS.path = Phantomjs.path
     @user_agents = Class.new.extend(Webdriver::UserAgent::Devices)
-    @base_desired_capabilities = {
+    @desired_capabilities = {
       'phantomjs.cli.args' => ["--proxy=#{proxy_host}:#{proxy_port}", '--proxy-type=socks5']
     }
   end
 
-  def random_user_agent
-    @user_agents.random_user_agent.chomp
-  end
-
   def create
-    desired_capabilities = {
-      'phantomjs.page.settings.userAgent' => random_user_agent
-    }
-    desired_capabilities.merge! @base_desired_capabilities
-    Selenium::WebDriver.for :phantomjs, desired_capabilities: desired_capabilities
+    Selenium::WebDriver.for :phantomjs, desired_capabilities: @desired_capabilities
   end
 end
 
